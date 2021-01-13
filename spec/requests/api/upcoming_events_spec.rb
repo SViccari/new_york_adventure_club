13ad8a7c659b1d4df12b92f7a60b2c6b054edb47
Stephanie Viccari <stephviccari@gmail.com>
2021-01-13 17:10:13 -0500

    Asserting against a performance improvement

--- spec/requests/api/upcoming_events_spec.rb
+++ spec/requests/api/upcoming_events_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe "GET /api/upcoming_events" do
   it "returns the next upcoming event" do
+    allow(HTTParty).to receive(:get).and_call_original
+
     get "/api/upcoming_events"
 
     expect(response).to be_successful
@@ -11,5 +13,7 @@ RSpec.describe "GET /api/upcoming_events" do
     expect(event["name"]).to match(/Exposing NYC's Gilded Age Elite Society/)
     expect(event["venue"]).to eq("Online event")
     expect(event["local_date"]).to eq("2021-01-13")
+
+    expect(HTTParty).to have_received(:get).once
   end
 end
