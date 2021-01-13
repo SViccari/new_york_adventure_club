b821b6029240e8fcbfb3daa1da6919a56243ec0a
Stephanie Viccari <stephviccari@gmail.com>
2021-01-13 13:27:38 -0500

    Stub the Adapter (HTTParty)

--- spec/models/meetup_event_spec.rb
+++ spec/models/meetup_event_spec.rb
@@ -3,25 +3,34 @@ require "rails_helper"
 RSpec.describe MeetupEvent do
   describe "#name" do
     it "returns the name" do
+      event_data = [ { "name" => "Foosball Game" } ]
+      expect(HTTParty).to receive(:get).and_return(event_data)
+
       meetup_event = MeetupEvent.new
 
-      expect(meetup_event.name).to match("'The Great Murals of New York City")
+      expect(meetup_event.name).to eq("Foosball Game")
     end
   end
 
   describe "#venue" do
     it "returns the venue name" do
+      event_data = [ { "venue" => { "name" => "Basement" } } ]
+      expect(HTTParty).to receive(:get).and_return(event_data)
+
       meetup_event = MeetupEvent.new
 
-      expect(meetup_event.venue).to eq("Online event")
+      expect(meetup_event.venue).to eq("Basement")
     end
   end
 
   describe "#date" do
     it "returns the event date" do
+      event_data = [ { "local_date" => "2021-01-15" } ]
+      expect(HTTParty).to receive(:get).and_return(event_data)
+
       meetup_event = MeetupEvent.new
 
-      expect(meetup_event.date).to eq("2021-01-13")
+      expect(meetup_event.date).to eq("2021-01-15")
     end
   end
 end
