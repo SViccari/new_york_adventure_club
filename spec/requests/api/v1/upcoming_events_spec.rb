5e5fd2c9e1da78e9c796192434f0c247c5dea0c6
Stephanie Viccari <stephviccari@gmail.com>
2021-01-13 13:14:58 -0500

    stub API request with WebMock

--- spec/requests/api/v1/upcoming_events_spec.rb
+++ spec/requests/api/v1/upcoming_events_spec.rb
@@ -2,14 +2,34 @@ require "rails_helper"
 
 RSpec.describe "GET /api/v1/upcoming_events" do
   it "returns the next upcoming event" do
+    event_data = [
+      {
+        name: "Foosball Game",
+        venue: { name: "Basement" },
+        local_date: "2021-01-15"
+      }
+    ]
+    stub_meetup_request(event_data)
+
     get "/api/v1/upcoming_events"
 
     event = JSON.parse(response.body)
 
     expect(response).to be_successful
 
-    expect(event["event_name"]).to match("'The Great Murals of New York City")
-    expect(event["event_venue"]).to eq("Online event")
-    expect(event["event_date"]).to eq("2021-01-13")
+    expect(event["event_name"]).to eq("Foosball Game")
+    expect(event["event_venue"]).to eq("Basement")
+    expect(event["event_date"]).to eq("2021-01-15")
+  end
+
+  def stub_meetup_request(event_data)
+    stub_request(
+      :get,
+      "https://api.meetup.com/NewYorkAdventureClub/events"
+    ).to_return(
+      status: 200,
+      headers: { content_type: 'application/json'},
+      body: event_data.to_json
+    )
   end
 end
