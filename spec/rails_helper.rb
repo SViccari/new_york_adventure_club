051b293425b9d81de58a84cf475723c06db18062
Stephanie Viccari <stephviccari@gmail.com>
2021-01-13 13:07:34 -0500

    add WebMock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'webmock/rspec'
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
