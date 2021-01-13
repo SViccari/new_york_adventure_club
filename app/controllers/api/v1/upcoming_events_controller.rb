fb55bcaffb55d70b022d1d89db54dc77582244b3
Stephanie Viccari <stephviccari@gmail.com>
2021-01-13 12:46:50 -0500

    shameless green - returning next event data

--- app/controllers/api/v1/upcoming_events_controller.rb
+++ app/controllers/api/v1/upcoming_events_controller.rb
@@ -2,7 +2,15 @@ module Api
   module V1
     class UpcomingEventsController < ApplicationController
       def index
-        render json: {}
+        render json: events_data
+      end
+
+      private
+
+      def events_data
+        HTTParty
+          .get("https://api.meetup.com/NewYorkAdventureClub/events")
+          .first
       end
     end
   end
--- spec/requests/api/v1/upcoming_events_spec.rb
+++ spec/requests/api/v1/upcoming_events_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe "GET /api/v1/upcoming_events" do
   it "returns the next upcoming event" do
     get "/api/v1/upcoming_events"
 
+    event = JSON.parse(response.body)
+
     expect(response).to be_successful
+
+    expect(event["name"]).to eq("'The Great Murals of New York City' Webinar")
+    expect(event["venue"]["name"]).to eq("Online event")
+    expect(event["local_date"]).to eq("2021-01-13")
   end
 end
