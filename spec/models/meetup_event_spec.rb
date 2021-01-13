c43d49ff9c38c3722b49bc5e6ed9da72963cd217
Stephanie Viccari <stephviccari@gmail.com>
2021-01-13 18:32:38 -0500

    Stub the Adapter (HTTParty)
    Summary:
    We also added a performance test to ensure the API call is memoized.

--- spec/models/meetup_event_spec.rb
+++ spec/models/meetup_event_spec.rb
@@ -3,26 +3,49 @@ require "rails_helper"
 RSpec.describe MeetupEvent do
   describe "#name" do
     it "returns the event name" do
+      allow(HTTParty)
+        .to receive(:get)
+        .and_return([{ "name" => "RSpec Training" }])
+
       meetup_event = MeetupEvent.new
 
-      expect(meetup_event.name)
-        .to match(/Exposing NYC's Gilded Age Elite Society/)
+      expect(meetup_event.name).to eq("RSpec Training")
     end
   end
 
   describe "#venue" do
     it "returns the event venue" do
+      allow(HTTParty)
+        .to receive(:get)
+        .and_return([{ "venue" => { "name" => "Mars" }}])
+
       meetup_event = MeetupEvent.new
 
-      expect(meetup_event.venue).to eq("Online event")
+      expect(meetup_event.venue).to eq("Mars")
     end
   end
 
   describe "#local_date" do
     it "returns the event date" do
+      allow(HTTParty)
+        .to receive(:get)
+        .and_return([{ "local_date" => "2000-01-13" }])
+
       meetup_event = MeetupEvent.new
 
-      expect(meetup_event.local_date).to eq("2021-01-13")
+      expect(meetup_event.local_date).to eq("2000-01-13")
     end
+  end
+
+  it "only issues the API call once" do
+    allow(HTTParty)
+      .to receive(:get)
+      .and_return([{"name" => "RSpec Training"}])
+
+    meetup_event = MeetupEvent.new
+    meetup_event.name
+    meetup_event.name
+
+    expect(HTTParty).to have_received(:get).once
   end
 end
--- test-times.md
+++ test-times.md
@@ -1,8 +1,9 @@
 First Iteration: Shameless Green, issuing real requests
 Time to run tests: 7.64 seconds
 
-Second Iteration: WebMock to stub out requests
-Time to run tests:
+Second Iteration:
+WebMock to stub out requests and stubbing the Adapter (HTTParty)
+Time to run tests: 0.06325 seconds
 
 
 Third Iteration: Stub the Adapter
